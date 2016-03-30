b12caa6da2ce332a8a9f5d942eb74cb9c732d5ea
TuoYu Zhang <ty5zhang@uwaterloo.ca>
2016-03-30 00:22:33 -0400

    assignment 7 both indexer and retrieval

---
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/nearalias/assignment7/BuildInvertedIndexHBase.java
@@ -0,0 +1,232 @@
+package ca.uwaterloo.cs.bigdata2016w.nearalias.assignment7;
+
+import java.io.IOException;
+import java.io.DataOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+
+import tl.lin.data.fd.Object2IntFrequencyDistribution;
+import tl.lin.data.fd.Object2IntFrequencyDistributionEntry;
+import tl.lin.data.pair.PairOfObjectInt;
+import tl.lin.data.pair.PairOfStringInt;
+
+public class BuildInvertedIndexHBase extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(BuildInvertedIndexHBase.class);
+
+  private static class MyMapper extends Mapper<LongWritable, Text, PairOfStringInt, IntWritable> {
+    private static final IntWritable COUNT = new IntWritable();
+    private static final PairOfStringInt KEY = new PairOfStringInt();
+    private static final Object2IntFrequencyDistribution<String> COUNTS = new Object2IntFrequencyDistributionEntry<String>();
+
+    @Override
+    public void map(LongWritable docno, Text doc, Context context) throws IOException, InterruptedException {
+      String text = doc.toString();
+
+      // Tokenize line.
+      List<String> tokens = new ArrayList<String>();
+      StringTokenizer itr = new StringTokenizer(text);
+      while (itr.hasMoreTokens()) {
+        String w = itr.nextToken().toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", "");
+        if (w.length() == 0) continue;
+        tokens.add(w);
+      }
+
+      // Build a histogram of the terms.
+      COUNTS.clear();
+      for (String token : tokens) {
+        COUNTS.increment(token);
+      }
+
+      // Emit postings.
+      for (PairOfObjectInt<String> e : COUNTS) {
+        KEY.set(e.getLeftElement(), (int) docno.get());
+        COUNT.set(e.getRightElement());
+        context.write(KEY, COUNT);
+      }
+    }
+  }
+
+  public static final String[] FAMILIES = { "p" };
+  public static final byte[] CF = FAMILIES[0].getBytes();
+
+  private static class MyTableReducer extends TableReducer<PairOfStringInt, IntWritable, ImmutableBytesWritable> {
+    public void reduce(PairOfStringInt key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      Put put = new Put(Bytes.toBytes(key.getLeftElement()));
+      for (IntWritable val : values) {
+        put.add(CF, Bytes.toBytes(key.getRightElement()), Bytes.toBytes(val.get()));
+      }
+      context.write(null, put);
+    }
+  }
+
+  private static class MyReducer extends Reducer<PairOfStringInt, IntWritable, Text, BytesWritable> {
+    private final static Text TERM = new Text();
+    private static String prevTerm = null;
+    private static int prevDocId = 0;
+    private static int df = 0;
+    private static ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
+    private static DataOutputStream dataOut = new DataOutputStream(byteArrayOut);
+
+    @Override
+    public void reduce(PairOfStringInt key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      Iterator<IntWritable> iter = values.iterator();
+      if (!(key.getLeftElement().equals(prevTerm)) && prevTerm != null) {
+        TERM.set(prevTerm);
+        WritableUtils.writeVInt(dataOut, df); // too lazy to go back and emit (word, *) so just gonna append df to the end pls don't fine me QAQ
+        context.write(TERM, new BytesWritable(byteArrayOut.toByteArray()));
+        byteArrayOut.reset();
+        dataOut.flush();
+        prevDocId = 0;
+        df = 0;
+      }
+      prevTerm = key.getLeftElement();
+      df ++;
+      WritableUtils.writeVInt(dataOut, key.getRightElement() - prevDocId);
+      WritableUtils.writeVInt(dataOut, iter.next().get());
+      prevDocId = key.getRightElement();
+    }
+
+    @Override
+    public void cleanup(Context context) throws IOException, InterruptedException {
+      TERM.set(prevTerm);
+      WritableUtils.writeVInt(dataOut, df);
+      context.write(TERM, new BytesWritable(byteArrayOut.toByteArray()));
+      prevTerm = null;
+      byteArrayOut.reset();
+      dataOut.flush();
+      dataOut.close();
+    }
+  }
+
+  private static class MyPartitioner extends Partitioner<PairOfStringInt, IntWritable> {
+    @Override
+    public int getPartition(PairOfStringInt key, IntWritable value, int numReduceTasks) {
+      return (key.getLeftElement().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
+  }
+
+  private BuildInvertedIndexHBase() {}
+
+  public static class Args {
+    @Option(name = "-config", metaVar = "[path]", required = true, usage = "HBase config")
+    public String config;
+
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    public String input;
+
+    @Option(name = "-table", metaVar = "[name]", required = true, usage = "HBase table to store output")
+    public String table;
+
+    @Option(name = "-reducers", metaVar = "[num]", required = false, usage = "number of reducers")
+    public int numReducers = 1;
+  }
+
+  /**
+   * Runs this tool.
+   */
+  public int run(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      return -1;
+    }
+
+    LOG.info("Tool: " + BuildInvertedIndexHBase.class.getSimpleName());
+    LOG.info(" - input path: " + args.input);
+    LOG.info(" - output table: " + args.table);
+    LOG.info(" - config: " + args.config);
+    LOG.info(" - number of reducers: " + args.numReducers);
+
+    // If the table doesn't already exist, create it.
+    Configuration conf = getConf();
+    conf.addResource(new Path(args.config));
+
+    Configuration hbaseConfig = HBaseConfiguration.create(conf);
+    HBaseAdmin admin = new HBaseAdmin(hbaseConfig);
+
+    if (admin.tableExists(args.table)) {
+      LOG.info(String.format("Table '%s' exists: dropping table and recreating.", args.table));
+      LOG.info(String.format("Disabling table '%s'", args.table));
+      admin.disableTable(args.table);
+      LOG.info(String.format("Droppping table '%s'", args.table));
+      admin.deleteTable(args.table);
+    }
+
+    HTableDescriptor tableDesc = new HTableDescriptor(TableName.valueOf(args.table));
+    for (int i = 0; i < FAMILIES.length; i++) {
+      HColumnDescriptor hColumnDesc = new HColumnDescriptor(FAMILIES[i]);
+      tableDesc.addFamily(hColumnDesc);
+    }
+    admin.createTable(tableDesc);
+    LOG.info(String.format("Successfully created table '%s'", args.table));
+
+    admin.close();
+
+    // Now we're ready to start running MapReduce
+    Job job = Job.getInstance(getConf());
+    job.setJobName(BuildInvertedIndexHBase.class.getSimpleName());
+    job.setJarByClass(BuildInvertedIndexHBase.class);
+
+    FileInputFormat.setInputPaths(job, new Path(args.input));
+    TableMapReduceUtil.initTableReducerJob(args.table, MyTableReducer.class, job);
+
+    job.setMapOutputKeyClass(PairOfStringInt.class);
+    job.setMapOutputValueClass(IntWritable.class);
+
+    job.setMapperClass(MyMapper.class);
+    job.setPartitionerClass(MyPartitioner.class);
+    job.setNumReduceTasks(args.numReducers);
+
+    long startTime = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+    return 0;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new BuildInvertedIndexHBase(), args);
+  }
+}
